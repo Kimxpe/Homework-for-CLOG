b5bd2d5171db0322243ce65d3345a3b5cea8e240
Kim <Kim@Kimberlys-MBP.home>
2016-09-12 11:54:57 -0400

    Week02Thursday: PhysicsSwitches

---
+++ CLOGS WORK/Assets/TrapdoorSwitch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapdoorSwitch : MonoBehaviour {
+	float rotationSpeed = -2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.E)) {
+			print ("You're free to jump!");
+			transform.Rotate (Vector3.right * -rotationSpeed);
+		}
+	}
+}
---
+++ CLOGS WORK/Assets/trapdoorSwitch2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trapdoorSwitch2 : MonoBehaviour {
+	float rotationSpeed = -2;
+
+	void Start () {
+	}
+
+	void Update () {
+		if (Input.GetKey(KeyCode.R)) {
+			print ("Welcome to the entrance of the cemetery of your lover...");
+			transform.Rotate (Vector3.up * -rotationSpeed);
+		}
+	}
+}
